106a722df73aa0a8d41e5851d34177be506dd4d6
abdoulfataho <yamyaogofataho@gmail.com>
2024-08-27 08:44:06 -0400

    SP7

---
+++ Glab-SP7/src/main/java/com/example/Glab_SP7/GlabSp7Application.java
@@ -0,0 +1,13 @@
+package com.example.Glab_SP7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlabSp7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlabSp7Application.class, args);
+	}
+
+}
---
+++ Glab-SP7/src/test/java/com/example/Glab_SP7/GlabSp7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Glab_SP7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlabSp7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
